3778efd8593060f90946fc65166c1b5b9b570ee5
Josh Miller <jmiller@twochicago.com>
2020-10-19 18:52:40 -0500

    removed usused code

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,2 @@
 const year = new Date().getFullYear();
 document.getElementById('copyrightYear').innerText = year;
-
-function(ConversionPercents) {
-    ConversionPercents.reduce((a, b) => (a + b)) / ConversionPercents.length;
-}
